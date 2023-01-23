0ef42dbdca757d916df78a4f06eb53dcf17d5869
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-01-23 03:47:15 +0300

    Working with the field and error output

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -9,6 +9,8 @@ type TodolistPropsType = {
     removeTask: (id: string) => void
     changeFilter: (value: FilterValuesType) => void
     addTask: (title: string) => void
+    changeStatus:(taskId: string, isDone: boolean) => void
+    filter:FilterValuesType
 }
 export type TaskType = {
     id: string
@@ -17,20 +19,29 @@ export type TaskType = {
 }
 export const Todolist = (props: TodolistPropsType) => {
     let [newTaskTitle, setNewTaskTitle] = useState('');
+
+    let [error , setError] = useState<string | null >(null)
+
+
+
+    const addTaskHandler = () => {
+        if(newTaskTitle.trim() !== ""){
+       props.addTask(newTaskTitle.trim());
+       setNewTaskTitle("")
+        }else {
+            setError('task is required')
+        }
+    }
+
     const onChangeInputHandler = (e: ChangeEvent<HTMLInputElement>) => {
         setNewTaskTitle(e.currentTarget.value)
     }
+
     const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
-   
+        setError(null);
         if(e.key === 'Enter'){
             addTaskHandler();
         }
-
-
-    }
-    const addTaskHandler = () => {
-        props.addTask(newTaskTitle);
-        setNewTaskTitle('');
     }
 
     const onMainChangeFilter = (value:FilterValuesType) => {
@@ -45,26 +56,31 @@ export const Todolist = (props: TodolistPropsType) => {
                     value={newTaskTitle}
                     onChange={onChangeInputHandler}
                     onKeyDown={onKeyPressHandler}
+                    className={error ?'error' : ""}
                 />
                 <button onClick={addTaskHandler}>+</button>
+                {error &&  <div className= 'error-message'>{error}</div>}
             </div>
             <ul>
                 {
                     props.tasks.map(el => {
                         const onRemoveHandler = () => props.removeTask(el.id);
+                        const onChangeInputHandler = (e:ChangeEvent<HTMLInputElement>) => {
+                            props.changeStatus(el.id , e.currentTarget.checked)
+                        }
                         return(
-                        <li key={el.id}>
+                        <li key={el.id}  className={el.isDone ? "is-Done" : ""}>
                             <button onClick={onRemoveHandler}>X</button>
-                            <input type="checkbox" checked={el.isDone}/>
+                            <input type="checkbox" onChange={onChangeInputHandler} checked={el.isDone}/>
                             <span>{el.title}</span>
                         </li>
                     )})
                 }
             </ul>
             <div>
-                <button onClick={()=> {onMainChangeFilter('all')}}>All</button>
-                <button onClick={()=> {onMainChangeFilter('active')}}>Active</button>
-                <button onClick={()=> {onMainChangeFilter('completed')}}>Completed</button>
+                <button className={props.filter === 'all'? "active-filter": ""} onClick={()=> {onMainChangeFilter('all')}}>All</button>
+                <button className={props.filter === 'active'? "active-filter": ""} onClick={()=> {onMainChangeFilter('active')}}>Active</button>
+                <button className={props.filter === 'completed'? "active-filter": ""} onClick={()=> {onMainChangeFilter('completed')}}>Completed</button>
             </div>
         </div>
 
