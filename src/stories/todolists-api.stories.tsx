f68c233357baccfc73a395e5458e23d7e9f4b5fc
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-03-26 23:47:08 +0300

    Working with server requests for todolists

---
+++ src/stories/todolists-api.stories.tsx
@@ -0,0 +1,55 @@
+import React, {useEffect, useState} from 'react'
+import {todolistsApi} from "../api/todolists-api";
+
+export default {
+    title: 'API'
+}
+
+export const GetTodolists = () => {
+    const [state, setState] = useState<any>(null)
+    useEffect(() => {
+        // здесь мы будем делать запрос и ответ закидывать в стейт.
+        // который в виде строки будем отображать в div-ке
+            todolistsApi.getTodolist()
+            .then((res) => {
+                setState(res.data)
+            })
+    }, [])
+    return <div>{JSON.stringify(state)}</div>
+}
+
+export const CreateTodolist = () => {
+    const [state, setState] = useState<any>(null)
+    useEffect(() => {
+        todolistsApi.createTodolist('React create todolist')
+            .then((res) => {
+                setState(res.data)
+            })
+    }, [])
+
+    return <div>{JSON.stringify(state)}</div>
+}
+export const DeleteTodolist = () => {
+    const [state, setState] = useState<any>(null)
+    useEffect(() => {
+        const todolistId = "78cf6079-7a35-4fcc-beb0-91f44a633a0e"
+        todolistsApi.deleteTodolist(todolistId)
+            .then((res) => {
+                setState(res.data)
+            })
+    }, [])
+
+    return <div>{JSON.stringify(state)}</div>
+}
+export const UpdateTodolistTitle = () => {
+    const [state, setState] = useState<any>(null)
+    useEffect(() => {
+        const todolistId = "78cf6079-7a35-4fcc-beb0-91f44a633a0e"
+        todolistsApi.updateTodolist(todolistId,'Angular create todolist')
+            .then((res) => {
+                setState(res.data)
+            })
+    }, [])
+
+    return <div>{JSON.stringify(state)}</div>
+}
