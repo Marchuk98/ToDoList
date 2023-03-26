0f327a8480ccae4cdf715cc418314f7bfcbbb4c0
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-03-26 23:48:32 +0300

    DAL level configuration and implementation of server requests

---
+++ src/api/todolists-api.ts
@@ -0,0 +1,81 @@
+import axios from "axios";
+
+const settings = {
+    withCredentials: true,
+    header: {
+        "API-KEY": "1fa14c67-71e4-4f59-9324-9f56be671b29"
+    }
+}
+
+const instance = axios.create({
+    baseURL:"https://social-network.samuraijs.com/api/1.1/",
+    ...settings
+})
+
+export type TodolistType = {
+    id: string,
+    title: string,
+    addedDate: string,
+    order: number
+}
+
+type ResponseType<D = {}> = {
+    resultCode: number
+    messages: Array<string>
+    fieldsErrors: Array<string>
+    data: D
+}
+
+export type TaskType = {
+    description: string
+    title: string
+    status: number
+    priority: number
+    startDate: string
+    deadline: string
+    id: string
+    todoListId: string
+    order: number
+    addedDate: string
+}
+
+export type UpdateTaskType = {
+    title: string
+    description: string
+    completed: boolean
+    status: number
+    priority: number
+    startDate: string
+    deadline: string
+}
+
+type GetTasksResponse = {
+    items: TaskType[]
+    totalCount: number
+    error: string | null
+}
+
+export const todolistsApi = {
+    getTodolist() {
+        return  instance.get<TodolistType[]>("todo-lists")
+
+    },
+    createTodolist(title: string) {
+        return  instance.post<ResponseType<{ item: TodolistType }>>("todo-lists", {title: title})
+
+    },
+    deleteTodolist(id: string) {
+        return  instance.delete<ResponseType>(`todo-lists/${id}`)
+
+    },
+    updateTodolist(id: string, title: string) {
+        return  instance.put<ResponseType>(`todo-lists/${id}`, {title: title})
+
+    },
+    getTasks(todolistId: string) {
+        return  instance.get<GetTasksResponse>(`todo-lists/${todolistId}/tasks`)
+    },
+    deleteTask(todolistId: string,taskId:string){
+        return instance.delete<ResponseType>(`todo-lists/${todolistId}/tasks/${taskId}`)
+    }
+}
